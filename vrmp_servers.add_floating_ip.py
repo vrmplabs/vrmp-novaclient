d18b2a9779c25456eac37a59ea5b6f45f3620ce2
tangliang0109 <tangliang@centrin.com.cn>
2012-05-10 13:49:18 +0800

    modify test.

--- vrmp_servers.add_floating_ip.py
+++ vrmp_servers.add_floating_ip.py
@@ -4,25 +4,66 @@ from novaclient.v1_1 import client
 nt = client.Client('admin', 'admin', 'admin', 'http://localhost:5000/v2.0', service_type='compute')
 
 # show serverlist
+serveridlist = []
 serverlist = nt.servers.list()
 print "Server list:"
 for server in serverlist:
     print "id:" + str(server.id) + "    name:" + server.name
+    serveridlist.append(server.id)
 
 # get server
 server = raw_input("Please enter the server:")
-while not server:
+while server not in serveridlist:
     server = raw_input("Please enter the server:")
 
 # show floating_iplist
+listlen = 0
+floating_ipiplist = []
 floating_iplist = nt.floating_ips.list()
-print "Floating_ip list:"
+print "Floating_ip list:" 
 for floating_ip in floating_iplist:
-    print "id:" + str(floating_ip.id) + "    name:" + floating_ip.name
+    if floating_ip.instance_id == None:
+        print "pool:" + floating_ip.pool + "    ip:" + floating_ip.ip
+        floating_ipiplist.append(floating_ip.ip)
+        listlen = listlen + 1
+
+createip = raw_input("Do you want to create a new floating ip? (y/n)")
+while createip not in ['y', 'n']:
+    createip = raw_input("Do you want to create a new floating ip? (y/n)")
+if createip == 'y':
+    # show floating_ip_poollist
+    floating_ip_poolnamelist = []
+    floating_ip_poollist = nt.floating_ip_pools.list()
+    print "Floating_ip_pool list:"
+    for floating_ip_pool in floating_ip_poollist:
+        print "name:" + floating_ip_pool.name
+        floating_ip_poolnamelist.append(floating_ip_pool.name)
+
+    # get pool
+    pool = raw_input("Please enter the pool:")
+    while pool and pool not in floating_ip_poolnamelist:
+        pool = raw_input("Please enter the pool:")
+
+    nt.floating_ips.create(pool=pool)
+
+    # show floating_iplist
+    floating_ipiplist = []
+    floating_iplist = nt.floating_ips.list()
+    print "Floating_ip list:"
+    for floating_ip in floating_iplist:
+        if floating_ip.instance_id == None:
+            print "pool:" + floating_ip.pool + "    ip:" + floating_ip.ip
+            floating_ipiplist.append(floating_ip.ip)
+else:
+    if listlen == 0:
+        print "VRMP_warn: No floating ip to be allocated, system has exit."
+        exit()
 
 # get address
 address = raw_input("Please enter the address:")
-while not address:
+while address not in floating_ipiplist:
     address = raw_input("Please enter the address:")
 
 nt.servers.add_floating_ip(server, address)
+
+print "VRMP_info: Allocate floating ip completed successfully."
--- vrmp_servers.create.py
+++ vrmp_servers.create.py
@@ -9,48 +9,61 @@ while not name:
     name = raw_input("Please enter the name:")
 
 # show imagelist
+imageidlist = []
 imagelist = nt.images.list()
 print "Image list:"
 for image in imagelist:
     print "id:" + str(image.id) + "    name:" + image.name
+    imageidlist.append(image.id)
 
 # get image
 image = raw_input("Please enter the image:")
-while not image:
+while image not in imageidlist:
     image = raw_input("Please enter the iamge:")
 
 # show flavorlist
+flavoridlist = []
 flavorlist = nt.flavors.list()
 print "Flavor list:"
 for flavor in flavorlist:
     print "id:" + str(flavor.id) + "    name:" + flavor.name
+    flavoridlist.append(flavor.id)
 
 # get flavor
 flavor = raw_input("Please enter the flavor(Size of image to launch.):")
-while not flavor:
+while flavor not in flavoridlist:
     flavor = raw_input("Please enter the flavor(Size of image to launch.):")
 
 # get userdata
 userdata = raw_input("Please enter the userdata:")
 
 # show keypairlist
+keypairnamelist = []
 keypairlist = nt.keypairs.list()
 print "Keypair list:"
 for keypair in keypairlist:
-    print "name:" + str(keypair.name)
+    print "name:" + keypair.name
+    keypairnamelist.append(keypair.name)
 
 # get key_name
 key_name = raw_input("Please enter the key_name(Which keypair to use for authentication.):")
+while key_name and key_name not in keypairnamelist:
+    key_name = raw_input("Please enter the key_name(Which keypair to use for authentication.):")
 
 # show security_grouplist
+security_groupnamelist = []
 security_grouplist = nt.security_groups.list()
 print "Security_group list:"
 for security_group in security_grouplist:
-    print "id:" + str(security_group.id) + "    name:" + security_group.name
+    print "name:" + security_group.name
+    security_groupnamelist.append(security_group.name)
 
 # get security_groups
 security_groups = raw_input("Please enter the security_groups(Launch instance in these security groups.):").split(',')
+for security_group in security_groups:
+    while security_group and security_group not in security_groupnamelist:
+        security_groups = raw_input("Please enter the security_groups(Launch instance in these security groups.):").split(',')
 
 server = nt.servers.create(name, image, flavor, userdata=userdata, key_name=key_name, security_groups=security_groups)
 
-print "Server " + server.name + " is creating, the id is '" + str(server.id)+"'."
+print "VRMP_info: Server '" + server.name + "' is creating, the id is '" + str(server.id) + "'."
